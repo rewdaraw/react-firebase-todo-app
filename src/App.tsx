fcabb6b6502cf5784218387024235ec0bf8ad431
rewdaraw <rewdaraw@gmail.com>
2021-05-31 11:09:37 +0900

    create new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { FormControl, TextField } from "@material-ui/core";
+import { AddToPhotos } from "@material-ui/icons";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -17,8 +20,29 @@ const App: React.FC = () => {
     return () => unSub();
   }, []);
 
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input });
+    // db登録後、新規の入力に備えて初期化する
+    setInput("");
+  };
+
   return (
     <div className="App">
+      <h1>Todo App by React</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          label="newtask"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotos />
+      </button>
+
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
       ))}
